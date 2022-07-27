6e8f38192eb481f321846d4ead0f6858dee548a8
usersploit <ankityadavmanish123@gmail.com>
2022-07-27 07:29:24 +0530

    localstorage

--- src/app/store.js
+++ src/app/store.js
@@ -4,17 +4,31 @@ import searchReducer from "../features/searchSlice";
 import { moviesApi } from "../features/Api";
 import idReducer from "../features/idSlice";
 import titleReducer from "../features/titleSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { combineReducers } from "redux";
 
-export const store = configureStore({
-  reducer: {
-    [moviesApi.reducerPath]: moviesApi.reducer,
-    search: searchReducer,
-    id: idReducer,
-    title: titleReducer,
-  },
+const reducers = combineReducers({
+  [moviesApi.reducerPath]: moviesApi.reducer,
+  search: searchReducer,
+  id: idReducer,
+  title: titleReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(moviesApi.middleware),
 });
 
 setupListeners(store.dispatch);
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import store from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
