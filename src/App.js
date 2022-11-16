c105e7d7fcd30b7cc402b3c303b2b603822db087
therogersak <therogersak@gmail.com>
2022-11-16 18:15:46 +0530

    :zap: add analysis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Suspense, lazy } from "react";
 import "./App.css";
+import ReactGA from "react-ga";
 import Header from "./Components/Header/Header";
 const Share = lazy(() => import("./Components/Share/Share"));
 const Home = lazy(() => import("./Components/Home/Home"));
+const TRACKING_ID = "4288168587"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
